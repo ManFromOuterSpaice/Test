ab9812cb562c1d3a232b75f1491ba247956a0dfc
Hafl <reck.aspnt@gmail.com>
2022-05-10 01:26:17 +0300

    Реализовал функцию draw_house TODO: функции draw_foundation, draw_walls, draw_roof

--- House.py
+++ House.py
@@ -1,8 +1,8 @@
 def main():
-    x, y = 100, 100
+    x, y = 300, 400
 
 
-    width, height = 200, 200
+    width, height = 200, 300
 
     draw_house(x, y, width, height)
 
@@ -19,10 +19,37 @@ def draw_house(x, y, width, height):
     """
     print('Типа рисую домик...', x, y, width, height)
     foundation_height = 0.05*height
-    draw_foundation(x, y, width, height)
-    draw_walls()
-    draw_roof()
-    pass    #do nothing
+    walls_width = 0.9 * width
+    walls_height = 0.5 * height
+    roof_height = height - foundation_height - walls_height
+
+    draw_foundation(x, y, width, foundation_height)
+    draw_walls(x, y - foundation_height, walls_width, walls_height)
+    draw_roof(x, y - foundation_height - walls_height, width, roof_height)
+
+
+def draw_foundation(x, y, width, height):
+
+  """
+    Нарисовать фундамент домика ширины widht и высоты height от опорной точки(x, y),
+    которая находится в середине нижней точки фундамента.
+    :param x: координата x середины фундамента
+    :param y: координата y низа фундамента
+    :param width: полная ширина фундамента (фундамент или вылеты крыши включены)
+    :param height: полная высота фундамента
+    :return: None
+    """
+  print("Фундамент",x,y,width,height)
+  pass  # do nothing
+
+def draw_walls(x, y, width,height):
+    print("Стены", x,y,width,height)
+    pass
+
+
+def draw_roof(x,y,widht, height):
+    print("Крыша",x,y,widht,height)
+    pass
 
 
 main()
